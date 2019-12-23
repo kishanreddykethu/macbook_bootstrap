178208cfa30ef68fdbf52640c7230bff656d2e9f
Kishan Reddy <kishanreddy.kethu@gmail.com>
2019-12-24 03:33:26 +0530

    Add logic to use symbolic link for tmux local config file

--- tmux/default.sh
+++ tmux/default.sh
@@ -3,4 +3,5 @@ cd ~
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cd -
-cp .tmux.conf.local ~/
+echo "Copying my custom config for tmux"
+ln -s $PWD/.tmux.conf.local ~/.tmux.conf.local
