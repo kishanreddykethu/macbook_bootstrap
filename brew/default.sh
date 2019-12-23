266e1c33265b7772a1366f77789b6e8f327f7408
Kishan Reddy <kishanreddy.kethu@gmail.com>
2019-12-24 04:20:18 +0530

    Add support for vagrant-vbguest

--- brew/default.sh
+++ brew/default.sh
@@ -4,4 +4,6 @@ echo "Installing usual cask leaves"
 xargs brew cask install < cask.txt
 echo "Installing usual brew leaves"
 xargs brew install < leaves.txt
-
+echo "Please verify and reinstall Virtualbox if it has failed"
+echo "Installing vagrant-vbguest here"
+vagrant plugin install vagrant-vbguest
